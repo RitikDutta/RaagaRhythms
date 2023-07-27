dd9f9f7bf05a1372d24bfb335d25c2765eead010
ritik dutta <ritikduttagd@gmail.com>
2023-07-27 22:54:49 +0530

    code formatting

--- script.js
+++ script.js
@@ -1,105 +1,78 @@
 var youTubePlayer;
-var vidElement = document.getElementById('YouTube-player');
-
+var vidElement = document.getElementById("YouTube-player");
 
 function toggleWidth() {
-    var checkbox = document.getElementById('toggleSlider');
-    var vidElement = document.getElementById('YouTube-player');
+    var checkbox = document.getElementById("toggleSlider");
+    var vidElement = document.getElementById("YouTube-player");
 
     // If the checkbox is checked, set the width to 400px, otherwise set it back to 200px
     if (checkbox.checked) {
         // If the checkbox is checked, set the position to 'fixed'
-        vidElement.style.position = 'fixed';
-        vidElement.style.top = '0'
-        vidElement.style.left = '0'
+        vidElement.style.position = "fixed";
+        vidElement.style.top = "0";
+        vidElement.style.left = "0";
     } else {
         // If the checkbox is unchecked, set the position to 'absolute'
-        vidElement.style.position = 'absolute';
+        vidElement.style.position = "absolute";
     }
 }
 
-
-
-
-
-vidElement.style.width="100%"
-vidElement.style.position = 'absolute'
-vidElement.style.top = '0'
-vidElement.style.left = '0'
-
+vidElement.style.width = "100%";
+vidElement.style.position = "absolute";
+vidElement.style.top = "0";
+vidElement.style.left = "0";
 
 function onYouTubeIframeAPIReady() {
-    'use strict';
+    "use strict";
 
-    var inputVideoId = document.getElementById('YouTube-video-id');
+    var inputVideoId = document.getElementById("YouTube-video-id");
     var videoId = inputVideoId.value;
-    var suggestedQuality = 'tiny';
+    var suggestedQuality = "tiny";
     var height = 250;
     var width = 400;
-    var youTubePlayerVolumeItemId = 'YouTube-player-volume';
-
+    var youTubePlayerVolumeItemId = "YouTube-player-volume";
 
     function onError(event) {
         youTubePlayer.personalPlayer.errors.push(event.data);
     }
 
-
     function onReady(event) {
         var player = event.target;
 
-        player.loadVideoById({suggestedQuality: suggestedQuality,
-                              videoId: videoId
-                             });
+        player.loadVideoById({ suggestedQuality: suggestedQuality, videoId: videoId });
         player.pauseVideo();
         youTubePlayerDisplayFixedInfos();
     }
-
 
     function onStateChange(event) {
         var volume = Math.round(event.target.getVolume());
         var volumeItem = document.getElementById(youTubePlayerVolumeItemId);
 
-        if (volumeItem && (Math.round(volumeItem.value) != volume)) {
+        if (volumeItem && Math.round(volumeItem.value) != volume) {
             volumeItem.value = volume;
         }
     }
 
-
-    youTubePlayer = new YT.Player('YouTube-player',
-                                  {videoId: videoId,
-                                   height: height,
-                                   width: width,
-                                   playerVars: {'autohide': 0,
-                                                'cc_load_policy': 0,
-                                                'controls': 0,
-                                                'disablekb': 1,
-                                                'iv_load_policy': 3,
-                                                'modestbranding': 1,
-                                                'rel': 0,
-                                                'showinfo': 0,
-                                                'start': 3
-                                               },
-                                   events: {'onError': onError,
-                                            'onReady': onReady,
-                                            'onStateChange': onStateChange
-                                           }
-                                  });
+    youTubePlayer = new YT.Player("YouTube-player", {
+        videoId: videoId,
+        height: height,
+        width: width,
+        playerVars: { autohide: 0, cc_load_policy: 0, controls: 1, disablekb: 1, iv_load_policy: 3, modestbranding: 1, rel: 0, showinfo: 0, start: 3 },
+        events: { onError: onError, onReady: onReady, onStateChange: onStateChange },
+    });
 
     // Add private data to the YouTube object
-    youTubePlayer.personalPlayer = {'currentTimeSliding': false,
-                                    'errors': []};
+    youTubePlayer.personalPlayer = { currentTimeSliding: false, errors: [] };
 }
-
 
 /**
  * :return: true if the player is active, else false
  */
 function youTubePlayerActive() {
-    'use strict';
+    "use strict";
 
-    return youTubePlayer && youTubePlayer.hasOwnProperty('getPlayerState');
+    return youTubePlayer && youTubePlayer.hasOwnProperty("getPlayerState");
 }
-
 
 /**
  * Get videoId from the #YouTube-video-id HTML item value,
@@ -107,20 +80,17 @@ function youTubePlayerActive() {
  * and show new infos.
  */
 function youTubePlayerChangeVideoId() {
-    'use strict';
+    "use strict";
 
-    var inputVideoId = document.getElementById('YouTube-video-id');
+    var inputVideoId = document.getElementById("YouTube-video-id");
     var videoId = inputVideoId.value;
 
-    youTubePlayer.cueVideoById({suggestedQuality: 'tiny',
-                                videoId: videoId
-                               });
+    youTubePlayer.cueVideoById({ suggestedQuality: "tiny", videoId: videoId });
     youTubePlayer.pauseVideo();
     youTubePlayerDisplayFixedInfos();
 
     youTubePlayer.playVideo();
 }
-
 
 /**
  * Seek the video to the currentTime.
@@ -129,35 +99,31 @@ function youTubePlayerChangeVideoId() {
  * :param currentTime: 0 <= number <= 100
  */
 function youTubePlayerCurrentTimeChange(currentTime) {
-    'use strict';
+    "use strict";
 
     youTubePlayer.personalPlayer.currentTimeSliding = false;
     if (youTubePlayerActive()) {
-        youTubePlayer.seekTo(currentTime*youTubePlayer.getDuration()/100, true);
+        youTubePlayer.seekTo((currentTime * youTubePlayer.getDuration()) / 100, true);
     }
 }
-
 
 /**
  * Mark that the HTML slider move.
  */
 function youTubePlayerCurrentTimeSlide() {
-    'use strict';
+    "use strict";
 
     youTubePlayer.personalPlayer.currentTimeSliding = true;
 }
-
 
 /**
  * Display embed code to #YouTube-player-fixed-infos.
  */
 function youTubePlayerDisplayFixedInfos() {
-    'use strict';
+    "use strict";
 
     if (youTubePlayerActive()) {
-        document.getElementById('YouTube-player-fixed-infos').innerHTML = (
-            'Embed code: <textarea readonly>' + youTubePlayer.getVideoEmbedCode() + '</textarea>'
-        );
+        document.getElementById("YouTube-player-fixed-infos").innerHTML = "Embed code: <textarea readonly>" + youTubePlayer.getVideoEmbedCode() + "</textarea>";
     }
 }
 
@@ -165,59 +131,44 @@ function youTubePlayerDisplayFixedInfos() {
  * Pause.
  */
 function youTubePlayerPause() {
-    'use strict';
+    "use strict";
 
     if (youTubePlayerActive()) {
         youTubePlayer.pauseVideo();
     }
 }
 
-
 /**
  * Play.
  */
 function youTubePlayerPlay() {
-    'use strict';
+    "use strict";
 
     if (youTubePlayerActive()) {
         youTubePlayer.playVideo();
     }
 }
 
-
-/**
- * Return the state decription corresponding of the state value.
- * If this value is incorrect, then return unknow.
- *
- * See values:
- * https://developers.google.com/youtube/iframe_api_reference#Playback_status
- *
- * :param number: any
- * :param unknow: any
- *
- * :return: 'unstarted', 'ended', 'playing', 'paused', 'buffering', 'video cued' or unknow
- */
 function youTubePlayerStateValueToDescription(state, unknow) {
-    'use strict';
+    "use strict";
 
-    var STATES = {'-1': 'unstarted',   // YT.PlayerState.
-                  '0': 'ended',        // YT.PlayerState.ENDED
-                  '1': 'playing',      // YT.PlayerState.PLAYING
-                  '2': 'paused',       // YT.PlayerState.PAUSED
-                  '3': 'buffering',    // YT.PlayerState.BUFFERING
-                  '5': 'video cued'};  // YT.PlayerState.CUED
+    var STATES = {
+        "-1": "unstarted", // YT.PlayerState.
+        "0": "ended", // YT.PlayerState.ENDED
+        "1": "playing", // YT.PlayerState.PLAYING
+        "2": "paused", // YT.PlayerState.PAUSED
+        "3": "buffering", // YT.PlayerState.BUFFERING
+        "5": "video cued",
+    }; // YT.PlayerState.CUED
 
-    return (state in STATES
-            ? STATES[state]
-            : unknow);
+    return state in STATES ? STATES[state] : unknow;
 }
-
 
 /**
  * Stop.
  */
 function youTubePlayerStop() {
-    'use strict';
+    "use strict";
 
     if (youTubePlayerActive()) {
         youTubePlayer.stopVideo();
@@ -225,47 +176,42 @@ function youTubePlayerStop() {
     }
 }
 
-
 /**
  * Change the volume.
  *
  * :param volume: 0 <= number <= 100
  */
 function youTubePlayerVolumeChange(volume) {
-    'use strict';
+    "use strict";
 
     if (youTubePlayerActive()) {
         youTubePlayer.setVolume(volume);
     }
 }
 
-
-
 /**
  * Main
  */
 (function () {
-    'use strict';
+    "use strict";
 
     function init() {
         // Load YouTube library
-        var tag = document.createElement('script');
+        var tag = document.createElement("script");
 
-        tag.src = 'https://www.youtube.com/iframe_api';
+        tag.src = "https://www.youtube.com/iframe_api";
 
-        var first_script_tag = document.getElementsByTagName('script')[0];
+        var first_script_tag = document.getElementsByTagName("script")[0];
 
         first_script_tag.parentNode.insertBefore(tag, first_script_tag);
-
 
         // Set timer to display infos
         setInterval(youTubePlayerDisplayInfos, 1000);
     }
 
-
     if (window.addEventListener) {
-        window.addEventListener('load', init);
+        window.addEventListener("load", init);
     } else if (window.attachEvent) {
-        window.attachEvent('onload', init);
+        window.attachEvent("onload", init);
     }
-}());
+})();
